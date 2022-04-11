4f4f0e847980c72d62683495c8aac7771fc873e7
rashel9454 <rashelrana931@gmail.com>
2022-04-11 23:53:13 +0600

    11/04/2022

---
+++ indext.php
@@ -0,0 +1,37 @@
+<h2>First Asainment</h2>
+<?php
+/***Question 01: 
+
+	We have an array [12,34,2,6,78]. What kind of array is this? Find all prime numbers from this array.
+
+Step 1: First let us find the factors of the given number( factors are the number that completely divides the given number)
+
+Step 2: Then check the total number of factors of that number
+
+Step 3: Hence, If the total number of factors is more than two, it is not a prime number but a composite number. Because When a number is divisible by only one and itself, then it is a prime number. */
+//Associative array//
+$prime_number=true;
+$all_arry = [12,34,2,6,78];
+foreach($all_arry as $kays => $number ){
+    for($i=2; $i <= $number-2; $i++){
+        if($number % $i ==0 ){
+            $prime_number=false;
+        }
+    }
+}
+if($prime_number){
+    echo "{$number} this is a prime number  ";
+}
+else{
+    echo "{$number} this is not prime number  ";
+}
+?>
+<h2>Third Asainment</h2>
+<?php
+/**Question 03: 
+
+		This is an array array(0,10,80,67,60,89,91,56,45,30,95,83,99) 
+
+Find the maximum value from this array.
+
+Must use a foreach loop. */
